8a8ab4b084b07a624c5c468718fb80361208b2ff
ffreemt <yucongo+fmt@gmail.com>
2019-09-17 13:02:02 +0800

    remove f-string to accommodate python3.5

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ r'''
 from pathlib import Path
 import re
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages  # type: ignore
 
 name = """google-tr-free"""
 # description = ' '.join(name.split('-'))
 description = name.replace('-tr-', ' translate for ')
 dir_name, = find_packages()
 
-version, = re.findall(r"\n__version__\W*=\W*'([^']+)'", open(Path(__file__).parent / f'{dir_name}/__init__.py').read())
+version, = re.findall(r"\n__version__\W*=\W*'([^']+)'", open('%s/__init__.py' % dir_name).read())
 
-README_rst = f'{Path(__file__).parent}/README.md'
+README_rst = '%s/README.md' % Path(__file__).parent
 long_description = open(README_rst, encoding='utf-8').read() if Path(README_rst).exists() else ''
 if Path('requirements.txt').exists():
     install_requires = [elm.split()[0] for elm in Path('requirements.txt').read_text(encoding='utf-8').split('\n')]
@@ -30,8 +30,8 @@ setup(
     long_description_content_type='text/markdown',
     keywords=['machine translation', 'free', 'scraping', ],
     author="mikeee",
-    url=f'http://github.com/ffreemt/{name}',
-    download_url='https://github.com/ffreemt/google-tr-free/archive/v_001.tar.gz',
+    url='http://github.com/ffreemt/%s' % name,
+    download_url='https://github.com/ffreemt/google-tr-free/archive/v_%s.tar.gz' % version,
     install_requires=install_requires,
     classifiers=[
         'Development Status :: 3 - Alpha',
